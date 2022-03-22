4af965a028726602904b9955816dd79c5bcaa75e
Umang <pro.umang3079@gmail.com>
2022-03-22 14:05:05 +0530

    prepopulate users and questions tables using model factories and  seed

--- database/migrations/2022_03_22_052757_create_questions_table.php
+++ database/migrations/2022_03_22_052757_create_questions_table.php
@@ -24,7 +24,7 @@ return new class extends Migration
             $table->unsignedInteger('best_answer_id')->nullable();
             $table->integer('user_id')->unsigned()->nullable();
             $table->timestamps();
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            // $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
